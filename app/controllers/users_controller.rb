e0fe0f602d0c85eddd204d185c4c8480ce5cba8a
Evan W <evanwang@mit.edu>
2014-12-11 02:54:15 +0000

    User creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
 
-  def new
+	# GET '/''
+	def new
+		@user = User.new
+	end
+
+	# POST '/users' (API)
+  def create
+
+  	@user = User.create(params[:user])
+  	
+  	# Success response
+  	render status: 200, :json => {
+  		:message => "Success!"
+  	}
+
   end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:email, :password)
+  	end
 
 end
