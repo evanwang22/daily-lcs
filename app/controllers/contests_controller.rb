0c77534d312c7e0a50fa590e5581a0edb0365a8f
Evan W <evanwang@mit.edu>
2014-12-19 02:46:45 +0000

    Force login for all routes in contests_controller

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,5 +1,7 @@
 class ContestsController < ApplicationController
 	
+  before_action :require_login
+
 	def show
 		@contest = Contest.find(params[:id])
 	end
@@ -11,5 +13,14 @@ class ContestsController < ApplicationController
 
 	def lobby
 	end
+
+  private
+ 
+ 		# Require logged in user before any actions in this controller
+	  def require_login
+	    unless current_user
+	      redirect_to root_path
+	    end
+	  end
 
 end
