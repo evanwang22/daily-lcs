341c4864ea9b31fb4717411fac9cf9b3676f229e
Evan W <evanwang@mit.edu>
2014-12-22 22:23:15 +0000

    Added entry controller with search route, updated entry model with search functions

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,26 @@
+class EntriesController < ApplicationController
+
+	# search entries by given params
+	# JSON
+	def index
+
+		@entries = Entries.all
+
+		@entries = @entries.by_contest(params[:contest_id]) if params[:contest_id].present? 
+		@entries = @entries.by_contest(params[:user_id]) if params[:user_id].present? 
+		@entries = @entries.by_contest(params[:lineup_id]) if params[:lineup_id].present?
+
+		render json: @entries 
+
+	end
+
+
+
+	private
+
+		# Not really sure if we need this if we never POST
+		# def user_params
+		# 	params.require(:user).permit(:email, :password, :username)
+		# end
+
+end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,11 @@
 class Entry < ActiveRecord::Base
-
 	belongs_to :contest
 	belongs_to :user
 	belongs_to :lineup
+
+	# Scopes (narrow DB queries) for searching entries by different fields
+	# just syntactic sugar for defining an actual class method
+	scope :by_contest, lambda { |contest_id| where(:contest_id => contest_id)}
+	scope :by_user, lambda { |user_id| where(:user_id => user_id)}
+	scope :by_lineup, lambda { |lineup_id| where(:lineup_id => lineup_id)}
 end
